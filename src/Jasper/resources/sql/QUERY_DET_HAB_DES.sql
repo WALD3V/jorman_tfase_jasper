34f56fdeed25c89fd8df0e505bdc72f359a1c770
Pc <walter.villamarr@gmail.com>
2026-02-27 01:53:06 -0500

    update forms login

--- src/Jasper/resources/sql/QUERY_DET_HAB_DES.sql
+++
@@ -1,3 +0,0 @@
--- Campos necesarios: tbl_codigo_per, tbl_codigo_fon, tbl_codigo_otr, tbl_codigo_cen
-SELECT tbl_codigo_per, tbl_codigo_fon, tbl_codigo_otr, tbl_codigo_cen
--- Retorna: Códigos de períodos, fondos, otros y centros
