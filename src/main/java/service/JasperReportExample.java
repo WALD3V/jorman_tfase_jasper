7265551dbae31a7debb556fadb3458f8f7462b82
Pc <walter.villamarr@gmail.com>
2026-02-27 01:55:25 -0500

    update forms login

--- src/main/java/service/JasperReportExample.java
+++ src/main/java/service/JasperReportExample.java
@@ -20,36 +20,35 @@ public class JasperReportExample {
     public static void generarReporte(RolGeneral rolGeneral, String fileName) {
         try {
             // Ruta del archivo .jasper compilado
-            String jasperPath = "src/Jasper/resources/jasperRolReport.jasper";
-            //ruta del logo de la empresa
-            String imagePath = "src/Jasper/resources/SampleIconImage.png";
-            
+            String jasperPath = "src/main/resources/jasperRolReport.jasper";
+            // ruta del logo de la empresa
+            String imagePath = "src/main/resources/SampleIconImage.png";
 
             // Los totales ya vienen calculados de las queries SQL
-
-
 
             // Convertir el objeto en una lista (Jasper necesita una colección)
             List<RolGeneral> listaRol = Collections.singletonList(rolGeneral);
 
             // Crear la fuente de datos para Jasper
             JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(listaRol);
-            // Crear datasources para las listas internas (suponiendo que getIngresos() y getEgresos() devuelven List<DetalleRol>)
+            // Crear datasources para las listas internas (suponiendo que getIngresos() y
+            // getEgresos() devuelven List<DetalleRol>)
             JRBeanCollectionDataSource ingresosDataSource = new JRBeanCollectionDataSource(rolGeneral.getIngresos());
             JRBeanCollectionDataSource egresosDataSource = new JRBeanCollectionDataSource(rolGeneral.getEgresos());
-            JRBeanCollectionDataSource descuentosDataSource = new JRBeanCollectionDataSource(rolGeneral.getDescuentos());
+            JRBeanCollectionDataSource descuentosDataSource = new JRBeanCollectionDataSource(
+                    rolGeneral.getDescuentos());
 
             // Parámetros adicionales (opcional)
             Map<String, Object> parameters = new HashMap<>();
             parameters.put("REPORT_LOCALE", Locale.US); // Pasar el locale como parámetro
-            //parameters.put("titulo", "Reporte de Rol General");
+            // parameters.put("titulo", "Reporte de Rol General");
 
             // Pasar las listas como parámetros
             parameters.put("ingresosDataSource", ingresosDataSource);
             parameters.put("egresosDataSource", egresosDataSource);
             parameters.put("descuentosDataSource", descuentosDataSource);
-            //pasar la direccion del logo como parametro
-            //parameters.put("ImagePath", imagePath);
+            // pasar la direccion del logo como parametro
+            // parameters.put("ImagePath", imagePath);
 
             // Llenar el reporte con los datos de rolGeneral
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperPath, parameters, dataSource);
